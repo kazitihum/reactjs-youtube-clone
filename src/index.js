a09364f1996a8a7e0e0da8032bd711a075ca65f5
aswinsanakan <aswinsanakan@gmail.com>
2017-08-28 12:32:18 +0530

    Add basic layout and youtube api key

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyBa5MifasWVBfsdxxbhFPNTAPvkRUTt26w'
+const App = () => {
+	return <div>Hello World !</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
